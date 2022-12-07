892cc39cc92d0b67c83167b6e46983fd200d01eb
John Torres <johnnyjamestorres@outlook.com>
2022-12-07 13:04:28 -0800

    Revert "Add event listeners for operators"
    This reverts commit e1ce359cd5db6dd974ac328f333eaf7df5f7304d.

--- scripts.js
+++ scripts.js
@@ -29,11 +29,6 @@ function operate(operator, a, b) {
   }
 }
 
-// Nums and result
-let num1, num2, result;
-
-// Display
-let needsCleared = false;
 const display = document.querySelector('#display');
 // TODO: prevent display content from overflowing
 
@@ -41,53 +36,12 @@ const display = document.querySelector('#display');
 const nums = document.querySelectorAll('.num');
 nums.forEach((num) =>
   num.addEventListener('click', () => {
-    if (needsCleared) {
-      clearDisplay();
-      needsCleared = false;
-    }
-    if (!display.textContent && num.textContent === '0') return;
     display.textContent += num.textContent;
   })
 );
 
 // Clear
-function clearDisplay() {
-  display.textContent = '';
-}
-function clearOperands() {
-  num1 = null;
-  num2 = null;
-}
-function clearOperator() {
-  curOp = null;
-}
-function clearAll() {
-  clearDisplay();
-  clearOperands();
-  clearOperator();
-}
 const clear = document.querySelector('#clear');
-clear.addEventListener('click', clearAll);
-
-// Operators
-let curOp;
-const operators = document.querySelectorAll('.operator');
-operators.forEach((operator) =>
-  operator.addEventListener('click', () => {
-    needsCleared = true;
-    num1 = parseInt(display.textContent);
-    curOp = operator.textContent;
-    console.log(curOp);
-  })
-);
-
-// Equal
-const equals = document.querySelector('#equal');
-equals.addEventListener('click', () => {
-  if (!curOp || isNaN(num1)) return;
-  needsCleared = true;
-  num2 = num2 ? num2 : parseInt(display.textContent);
-  result = operate(curOp, num1, num2);
-  num1 = result;
-  display.textContent = result;
+clear.addEventListener('click', () => {
+  display.textContent = '';
 });
